29c8d9c0daa7b7230ed308db75c60255e3f292a6
Rishab Sharma <46566471+Rishab1207@users.noreply.github.com>
2021-03-06 14:38:01 +0530

    Removed links

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const puppeteer = require("puppeteer");
 (async () => {
 	const browser = await puppeteer.launch({ headless: false });
 	const page = await browser.newPage();
-	await page.goto("https://www.redbubble.com/auth/login", {
+	await page.goto("", {
 		waitUntil: "networkidle2",
 		timeout: 0,
 	});
 
-	await page.type("#ReduxFormInput1", "basketballbyte", { delay: 200 });
+	await page.type("#ReduxFormInput1", "", { delay: 200 });
 
 	await page.click(".app-ui-components-Button-Button_button_1_MpP", {
 		delay: 100,
